3d26ded4a626505343c7ac75c701fa8734c01bb4
Aspen Wilson <aspen.schell@gmail.com>
2022-12-15 12:59:35 -0800

    commit tree 0c8edc9a6649f8262caf51682f1582168139d192

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){cats.push('Ralph');}
+
+function destructivelyPrependCat(){cats.unshift('Bob');}
+
+function destructivelyRemoveLastCat(){cats.pop();}
+
+function destructivelyRemoveFirstCat(){cats.shift();}
+
+function appendCat(){const copyofCats=[...cats,'Broom'];return copyofCats}
+
+function prependCat(){const copyofCats=['Arnold', ...cats];return copyofCats}
+
+function removeLastCat(){const copyofCats=cats.slice(0,2); return copyofCats}
+
+function removeFirstCat(){const copyofCats=cats.slice(1); return copyofCats}
+
+
+
